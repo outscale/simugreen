3cb4cb83f576f6aca5397ce64ba62253a35ebb74
arthur.vervaet <arthur.vervaet@outscale.com>
2022-09-26 17:27:21 +0200

    add example methods

--- app1/src/app1.py
+++ app1/src/app1.py
@@ -9,6 +9,24 @@ CONFIG_FILE = "/tmp/app1.cfg"
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
 
+# Below you will find demo func don't hesitate to try this code using example_input.json as input file
+def funcA(arg1, arg2, arg3=""):
+    """
+    This is an example function
+    Concatenate multiple string, note that arg3 is optional
+    """
+    return f"{arg1}_{arg2}{arg3}"
+
+def funcB(arg1=None):
+    """
+    This is an example function
+    Check if an argument was passed. if so return it else return default message
+    """
+    if arg1:
+        return arg1
+    else:
+        return "Not filled !"
+
 def main():
     while True:
 
@@ -25,13 +43,26 @@ def main():
         with open(os.path.join(config['FILE']['INPUT'], file), 'r') as f:
             commands = json.load(f)
         
-        tmp_path = f"/tmp/app1/{os.path.splitext(file)[0]}.txt"
+        tmp_path = f"/tmp/{os.path.splitext(file)[0]}.txt"
+
         # For each command within the file perform an action
         with open(tmp_path, 'w') as f:
             for id, command in commands.items():
+
                 # !!!!!!!!!!!!!!!!!!!!!!!! WIP !!!!!!!!!!!!!!!!!!!
-                output = "placeholder" # replace this lines by operation depanding of the command type
+                # This is where you can add specific processing for each command type
+
+                if command.get("type") == "typeA":
+                    output = funcA(**command.get("arguments"))
+
+                elif command.get("type") == "typeB":
+                    output = funcB(**command.get("arguments"))
+
+                else:
+                    output = f"{command.get('type')} not handled"
+
                 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
                 f.write(f"{id} {output}\n")
 
         # Once the file is processed delete it
